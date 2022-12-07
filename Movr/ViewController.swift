afa913bfbdd795a60835b1adf33aa26fa1e7104e
Waheed Afolabi <waheed.remote247@gmail.com>
2022-12-07 14:00:04 +0100

    Initial Commit

---
+++ Movr/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Movr
+//
+//  Created by waheedCodes on 07/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
