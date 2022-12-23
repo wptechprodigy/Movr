145fe2deb6c2eb78c1209c0183c34d4101589dfe
Waheed Afolabi <waheed.remote247@gmail.com>
2022-12-23 13:03:31 +0100

    Design the location input activator view

---
+++ Movr/Screens/Home/Views/LocationInputActivationView.swift
@@ -0,0 +1,59 @@
+//
+//  LocationInputActivationView.swift
+//  Movr
+//
+//  Created by waheedCodes on 23/12/2022.
+//
+
+import UIKit
+
+class LocationInputActivationView: UIView {
+    
+    // MARK: - Properties
+    
+    private let indicatorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        return view
+    }()
+    
+    private let placeholderLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Where to?"
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.textColor = .systemGray5
+        return label
+    }()
+    
+    // MARK: - Initializers
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpSubviews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUpSubviews() {
+        backgroundColor = .white
+        
+        setSelfAttributes()
+        
+        addSubview(indicatorView)
+        indicatorView.centerY(inView: self, leftAnchor: leftAnchor, paddingLeft: 16)
+        indicatorView.setDimensions(width: 6, height: 6)
+        
+        addSubview(placeholderLabel)
+        placeholderLabel.centerY(inView: self, leftAnchor: indicatorView.rightAnchor, paddingLeft: 20)
+    }
+    
+    private func setSelfAttributes() {
+        layer.cornerRadius = 8
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOpacity = 0.55
+        layer.shadowOffset = CGSize(width: 0.5, height: 0.5)
+        layer.masksToBounds = false
+    }
+}
